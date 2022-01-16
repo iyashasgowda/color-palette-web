dceb125c0f4d8e2c36fb65bb78f2f4d4663e4c72
Yashas Gowda <yashas.p@ibridgellc.com>
2022-01-16 13:24:32 +0530

    #fixed icon sizing issue

--- src/components/section/header/Header.jsx
+++ src/components/section/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Header = (props) => {
-   let theme = props.darkMode ? `${process.env.PUBLIC_URL}/assets/icons/dark/dark.svg` : `${process.env.PUBLIC_URL}/assets/icons/light/dark.svg`;
+   let theme = props.darkMode ? `${process.env.PUBLIC_URL}/assets/icons/dark/dark.svg` : `${process.env.PUBLIC_URL}/assets/icons/light/light.svg`;
    return (
       <>
          <header className='header'>
             <div>
                <h1>Color Palette</h1>
-               <p>v{props.version}</p>
+               <p>{`v${props.version} beta`}</p>
             </div>
 
             <img src={theme} alt='theme' onClick={props.changeTheme}></img>
