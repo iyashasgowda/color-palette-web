6be082f5f68d74580dbd8d252754d162ec4a7fd9
Yashas Gowda <yashas348@gmail.com>
2022-06-30 18:32:47 +0530

    #added theme control & request feature to settings

--- src/components/comms/Settings.jsx
+++ src/components/comms/Settings.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 
-import data from '../../utils/data.json';
 import '../../css/slider.css';
+import author from '../../utils/author.json';
+import data from '../../utils/data.json';
 import { makeToast, updateSlider } from '../../utils/utils';
 
 const Settings = (props) => {
    const close_img = props.darkMode ? `${process.env.PUBLIC_URL}/assets/icons/dark/close.svg` : `${process.env.PUBLIC_URL}/assets/icons/light/close.svg`;
+   const email_img = props.darkMode ? `${process.env.PUBLIC_URL}/assets/icons/light/email.svg` : `${process.env.PUBLIC_URL}/assets/icons/dark/email.svg`;
 
    useEffect(() => updateSlider('palette_seekbar', data.slider.alpha));
 
@@ -14,6 +16,11 @@ const Settings = (props) => {
       props.changeExtractionCount(e.target.value);
       if (e.target.value <= 2) makeToast('Minimum 2 colors are required!');
    };
+
+   const sendEmail = () => {
+      const link = `mailto:${author.email}?subject=${encodeURIComponent('Color Palette: Request Feature')}`;
+      window.location.href = link;
+   }
 
    return (
       <div className='settings-modal'>
@@ -24,8 +31,18 @@ const Settings = (props) => {
             </div>
 
             <div className='settings-modal-body'>
+               <h3>Theme</h3>
+               <div className='settings-category theme'>
+                  <div className='title-desc'>
+                     <h4>Dark Mode</h4>
+                     <p>Improves visibility for users with low vision and those who are sensitive to bright light.</p>
+                  </div>
+
+                  <input type='checkbox' className='toggle-checkbox' id='solid-alpha-checkbox' checked={props.darkMode} onChange={() => props.changeTheme()} />
+               </div>
+
                <h3>Palette</h3>
-               <div className='extract-count'>
+               <div className='settings-category palette'>
                   <p>Set number of colors you want to extract:</p>
 
                   <div className='palette_seekbar_count'>
@@ -37,6 +54,12 @@ const Settings = (props) => {
                   <p className='info'>
                      <b>Note:</b> The number of colors extracted might be lower than the value you've set. This is due to that particular image and it is normal.
                   </p>
+               </div>
+
+               <h3>Feedback</h3>
+               <div className='settings-category feedback'>
+                  <p>Request developer to implement your required feature.</p>
+                  <img src={email_img} alt='email' onClick={() => sendEmail()} />
                </div>
             </div>
          </div>
